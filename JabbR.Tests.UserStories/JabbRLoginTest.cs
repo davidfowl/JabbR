5addd083d1ed22572a50e4537faa1b25b0969f53
sachin303 <sachin303@gmail.com>
2013-04-13 23:26:43 +0530

    refactoring to meet jabbR coding guidelines and merge user stories target in main build

--- JabbR.Tests.UserStories/JabbRLoginTest.cs
+++ JabbR.Tests.UserStories/JabbRLoginTest.cs
@@ -7,34 +7,34 @@ namespace JabbR.Tests.UserStories
 {
     public class JabbRLoginTest : IDisposable
     {
-        private OpenQA.Selenium.Firefox.FirefoxDriver driver;
-        private JabbRUserStoryHelper helper = new JabbRUserStoryHelper();
-
-        private const string jabbrBaseUrl = "http://localhost:16207/";
-
-        private const string testuser1 = "testuser1";
-        private const string testuser1pwd = "testuser1";
-        private const string testuser1email = "abc@xyz.com";
+        private FirefoxDriver _driver;
+        
+        private const string _jabbrBaseUrl = "http://localhost:16207/";
+        private const string _testuser1 = "testuser1";
+        private const string _testuser1pwd = "testuser1";
+        
+        //private const string testuser1email = "abc@xyz.com";
+        //private JabbRUserStoryHelper _helper = new JabbRUserStoryHelper( );
 
         public JabbRLoginTest()
         {
-            driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl(jabbrBaseUrl);
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(_jabbrBaseUrl);
         }
 
         [Fact]
         public void LoginFailedWithoutUserRegistration()
         {
-            var element = driver.FindElement(By.Name("username"));
-            element.SendKeys(testuser1);
+            var element = _driver.FindElement(By.Name("username"));
+            element.SendKeys(_testuser1);
 
-            var pwd = driver.FindElement(By.Name("password"));
-            pwd.SendKeys(testuser1pwd);
+            var pwd = _driver.FindElement(By.Name("password"));
+            pwd.SendKeys(_testuser1pwd);
 
-            IWebElement submitBtn = driver.FindElement(By.ClassName("btn"));
+            IWebElement submitBtn = _driver.FindElement(By.ClassName("btn"));
             submitBtn.Click();
 
-            var result = driver.ExecuteScript("return $('li',$('.validation-summary-errors')).text();");
+            var result = _driver.ExecuteScript("return $('li',$('.validation-summary-errors')).text();");
 
 
             Assert.Equal("Login failed. Check your username/password.", result);
@@ -44,7 +44,7 @@ namespace JabbR.Tests.UserStories
 
         public void Dispose()
         {
-            driver.Dispose();
+            _driver.Dispose();
         }
     }
 }
--- JabbR.Tests.UserStories/JabbRUserStoryBase.cs
+++ JabbR.Tests.UserStories/JabbRUserStoryBase.cs
@@ -1,13 +1,13 @@
 ﻿using JabbR.Models;
 using JabbR.Services;
 
-namespace JabbR.Tests.UserStories {
-    
+namespace JabbR.Tests.UserStories 
+{   
     public class JabbRUserStoryHelper
     {
-        private   IMembershipService membershipService;
-        private IJabbrRepository repository;
-        public ICryptoService criptoService;
+        private   IMembershipService _membershipService;
+        private IJabbrRepository _repository;
+        public ICryptoService _criptoService;
 
         public JabbRUserStoryHelper()
         {
@@ -16,16 +16,16 @@ namespace JabbR.Tests.UserStories {
 
         public void Setup()
         {
-            repository = new PersistedRepository(new JabbrContext());
-            criptoService = new CryptoService(new FileBasedKeyProvider());
+            _repository = new PersistedRepository(new JabbrContext());
+            _criptoService = new CryptoService(new FileBasedKeyProvider());
 
-            membershipService = new MembershipService(repository, criptoService);
+            _membershipService = new MembershipService(_repository, _criptoService);
         }
 
         
         public void SetupNewUser(string userName, string password, string email)
         {
-            membershipService.AddUser(userName, email, password);
+            _membershipService.AddUser(userName, email, password);
         }
     }
 }
