85e3090e9beafbcbe2e1b18e711a7b48b10eb596
sachin303 <sachin303@gmail.com>
2013-04-14 00:56:21 +0530

    remove unwanted sql scripts

--- SQLScripts/resetdb.sql
+++
@@ -1,8 +0,0 @@
-Delete from attachments;
-Delete from ChatClients;
-Delete from ChatMessages;
-Delete from ChatRoomChatUsers;
-Delete from chatuserchatrooms;
-Delete from chatuseridentities;
-Delete from chatrooms;
-Delete from chatusers;
