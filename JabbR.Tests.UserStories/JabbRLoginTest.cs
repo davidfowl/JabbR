320e78c7525e0ac3b6838b5031ccb59c5927a29f
sachin303 <sachin303@gmail.com>
2013-04-16 23:58:34 +0530

    Replace selenium web driver with simple Browser (headless)

--- JabbR.Tests.UserStories/JabbRLoginTest.cs
+++ JabbR.Tests.UserStories/JabbRLoginTest.cs
@@ -1,41 +1,41 @@
 ﻿using System;
+using SimpleBrowser;
+using SimpleBrowser.WebDriver;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+//using OpenQA.Selenium.Firefox;
 using Xunit;
 
 namespace JabbR.Tests.UserStories
 {
     public class JabbRLoginTest : IDisposable
     {
-        private FirefoxDriver _driver;
-        
-        private const string _jabbrBaseUrl = "http://localhost:16207/";
+        private SimpleBrowserDriver _driver;
+
+        private const string _jabbrLoginUrl = "http://localhost:16207/#/rooms/master";
         private const string _testuser1 = "testuser1";
         private const string _testuser1pwd = "testuser1";
         
         public JabbRLoginTest()
         {
-            _driver = new FirefoxDriver();
-            _driver.Navigate().GoToUrl(_jabbrBaseUrl);
+            _driver = new SimpleBrowserDriver();
+            _driver.Navigate( ).GoToUrl( _jabbrLoginUrl );
         }
 
         [Fact]
         public void LoginFailedWithoutUserRegistration()
         {
-            var element = _driver.FindElement(By.Name("username"));
+            var element = _driver.FindElement(By.Id("username"));
             element.SendKeys(_testuser1);
 
             var pwd = _driver.FindElement(By.Name("password"));
             pwd.SendKeys(_testuser1pwd);
 
-            IWebElement submitBtn = _driver.FindElement(By.ClassName("btn"));
+            var submitBtn = _driver.FindElement(By.ClassName("btn"));
             submitBtn.Click();
 
-            var result = _driver.ExecuteScript("return $('li',$('.validation-summary-errors')).text();");
-
+            var result = _driver.FindElement(By.CssSelector(".validation-summary-errors > li")).Text;
 
             Assert.Equal("Login failed. Check your username/password.", result);
-
         }
 
         public void Dispose()
